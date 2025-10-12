fe8c8c73f6b7af072943bdfeb6b8831b3b451e4a
Pigbuy <felixtk@web.de>
2025-10-12 23:39:50 +0200

    people.py should be functional now lol

--- src/filter_types/filter_arg_types/people.py
+++ src/filter_types/filter_arg_types/people.py
@@ -6,8 +6,23 @@ from filter_types.filter_arg_types.filter_arg_type import FilterArgType
 class People(FilterArgType):
     @staticmethod
     def validate_str(string:str) -> bool:
+        people_path:Path =  Path(args.faces_location)
+        valid_people: list[str] = []
+        for f in people_path.iterdir():
+            name = f.name
+            if f.is_file() and "." in name:
+                name = name.split(".")[0]
+            valid_people.append(name)
+        
+        people_to_check = string.split(",")
+        for person in people_to_check:
+            if person not in valid_people:
+                return False
         return True
+    
     def parse_valid_string(self, valid_string) -> "FilterArgType":
+        self.people = valid_string.split(",")
         return self
+    
     def __init__(self, people:tuple[str]) -> None:
         self.people = people
