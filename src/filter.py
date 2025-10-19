49641c8ea85d9184b2d66923511ebbdd8b8363b3
Pigbuy <felixtk@web.de>
2025-10-19 08:11:37 +0200

    idk I forgot what I did here

--- src/filter.py
+++ src/filter.py
@@ -6,20 +6,20 @@ from typing import Type
 
 class Filter():
     """class that, when instantiated, contains all information about a specific filter defined in the configuration"""
-    def __init__(self, filter_name: str, filter_type_name:str) -> "Filter":
+    def __init__(self, filter_name: str, filter_type_name:str) -> None:
         self.filter_name = filter_name
 
         self.filter_type:FilterTypes = FilterTypes[filter_name]
         
-
-        return self
     
     def filter(self, img) -> bool:
         return False
     
 class FilterGroup():
     """class that, when instantiated, contains multiple filters and the ability to sort an image according to these filters into a category or assign it to an attribute"""
-    pass
+    def __init__(self, name, filters:tuple[Filter]) -> None:
+        self.name = name
+        self.filters = filters
 
 class Category(FilterGroup):
     pass
