c5b81e6d9aee1c2ecf6be9bd9924cb3a9cef4370
Pigbuy <felixtk@web.de>
2025-10-29 21:09:08 +0100

    added description

--- src/filter_types/filter_arg_types/filter_arg_type.py
+++ src/filter_types/filter_arg_types/filter_arg_type.py
@@ -1,5 +1,8 @@
 
 class FilterArgType():
+    """A filter argument type is an argument type that is used by a filter/filter type.
+    It can validate and parse that type from text in the configuration file and can then be used by the filter type
+    to parse its arguments which may be multiple of different types. This type of class may be instantiated for ease of use."""
     @staticmethod
     def validate_str(string:str) -> str:
         return "" #return "" if valid, error mesage if not
