22ac7694a3baeb3c943ae1e8b9e7cbc5870c6ada
Pigbuy <felixtk@web.de>
2025-11-24 03:23:18 +0100

    added the ability to add another reason to the error

--- src/Errors.py
+++ src/Errors.py
@@ -1,4 +1,5 @@
 from contextlib import contextmanager
+from dataclasses import dataclass
 import textwrap
 
 # ANSI color codes
@@ -17,10 +18,10 @@ def get_current_tree():
         tree = tree[p]
     return tree
 
-class Error:
-    def __init__(self, branch_path: list[str], description: str) -> None:
-        self.branch_path = branch_path
-        self.desc = description
+@dataclass
+class ErrorRecord:
+    branch_path: list[str]
+    reason: str
 
 class Errors:
     @staticmethod
@@ -36,12 +37,21 @@ class Errors:
             CURRENT_ERR_TREE_POS.pop()
 
     @staticmethod
-    def queue_error(name: str, description: str):
-        get_current_tree()[name] = description
+    def queue_error(name: str, reason: str):
+        get_current_tree()[name] = reason
+    
+    @staticmethod
+    def add_error_reason(name: str, reason: str):
+        tree = get_current_tree()
+        if tree[name] == "":
+            tree[name] = reason
+        else:
+            tree[name] = tree[name] + " and " + reason
+
 
     @staticmethod
     def throw_if_errors():
-        errs_to_throw: list[Error] = []
+        errs_to_throw: list[ErrorRecord] = []
 
         def recurse(tree: dict, branch_path: list[str]):
             for key, value in tree.items():
@@ -49,7 +59,7 @@ class Errors:
                 if isinstance(value, dict):
                     recurse(value, new_branch_path)
                 else:
-                    errs_to_throw.append(Error(new_branch_path, value))
+                    errs_to_throw.append(ErrorRecord(new_branch_path, value))
 
         recurse(ERROR_TREE, [])
 
@@ -59,11 +69,11 @@ class Errors:
         msg = f"{YELLOW}\n\nTHE FOLLOWING {len(errs_to_throw)} ERROR(S) OCCURRED:{RESET}"
 
         for i, error in enumerate(errs_to_throw, 1):
-            err_msg = f"{WHITE}----{i}-----{RESET}\n{RED}{error.branch_path[-1]}{RESET}\n"
+            err_msg = f"{WHITE}-----{i}-----{RESET}\n{RED}{error.branch_path[-1]}{RESET}\n"
             err_path = error.branch_path[:-1]
             for bn in reversed(err_path):
-                err_msg += textwrap.indent(f"{WHITE}while{RESET}\n    {CYAN}{bn}{RESET}\n", "    ")
-            err_msg += f"\n{WHITE}because {RED}{error.desc}{RESET}\n"
+                err_msg += textwrap.indent(f"{WHITE}while{RESET} {CYAN}{bn}{RESET}\n", "    ")
+            err_msg += f"{WHITE}because {RED}{error.reason}{RESET}\n"
             msg += "\n" + err_msg
 
         raise Exception(msg)
